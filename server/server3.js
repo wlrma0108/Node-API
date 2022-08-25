7110fb44eacd2f2e0cd1e80ded843cd97a47ca9c
wlrma0108 <dlwlrma0108@gmail.com>
2022-08-25 14:42:17 +0900

    commit

--- server/server3.js
+++ server/server3.js
@@ -1,27 +1,4 @@
-
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const parseCookies = (cookie ='') =>
-cookie
-.split(';')
-.map(v => v.split('='))
-.map(([k, ...vs]) => [k, vs.join('=')])
-.reduce((acc, [k, v]) => {
-acc[k.trim()] = decodeURIComponent(v);
-return acc;
-}, {});
-const session = {};
-http.createServer((req, res) => {
-const cookies = parseCookies(req.headers.cookie);
-if (req.url.startsWith('/login')) {
-const { query } = url.parse(req.url);
-const { name } = qs.parse(query);
-const expires = new Date();
-expires.setMinutes(expires.getMinutes() + 5);
-const randomInt = +new Date();
-session[randomInt] = {
-name,
-expires,
-};
+const users = {};
+
