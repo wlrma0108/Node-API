05613591141b8fdc1ac459d9c4539e1329dab7be
wlrma0108 <dlwlrma0108@gmail.com>
2022-08-28 23:35:02 +0900

    commit

--- node_js_base/app.js
+++ node_js_base/app.js
@@ -30,3 +30,23 @@ res.status(err.status || 500);
 res.render('error');
 });
 module.exports = app;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// 404 처리 미들웨어
+app.use(function(req, res, next) {
+next(createError(404));
+});
+// 에러 핸들러
+app.use(function(err, req, res, next) {
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') ==='development' ? err : {};
+res.status(err.status || 500);
+res.render('error');
+});
+module.exports = app;
